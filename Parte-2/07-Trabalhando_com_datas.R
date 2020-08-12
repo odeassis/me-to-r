8b0ff0e8a1fb702f0765c69b1e10c4f23817dbfc
fdAssis <fd07ass@gmail.com>
2020-08-11 23:18:15 -0300

    parte2/trabalhando com datas

--- Parte-2/07-Trabalhando_com_datas.R
+++ Parte-2/07-Trabalhando_com_datas.R
@@ -83,3 +83,61 @@ b2
 b2 - b1
 
 difftime(b2, b1, units='weeks')
+
+# Pacote lubridate
+?lubridate
+install.packages("lubridate")
+require(lubridate)
+
+ymd("20160604") 
+mdy("06-04-2016") 
+dmy("04/06/2016")
+
+chegada <- ymd_hms("2016-06-04 12:00:00", tz = "Pacific/Auckland")
+partida <- ymd_hms("2011-08-10 14:00:00", tz = "Pacific/Auckland")
+
+chegada
+partida
+
+second(chegada)
+second(chegada) <- 23
+chegada
+wday(chegada)
+wday(chegada, label = TRUE)
+class(chegada)
+interval(chegada, partida)
+
+
+tm1.lub <- ymd_hms("2016-05-24 23:55:26")
+tm1.lub
+
+tm2.lub <- mdy_hm("05/25/16 08:32")
+tm2.lub
+
+year(tm1.lub)
+week(tm1.lub)
+
+tm1.dechr <- hour(tm1.lub) + minute(tm1.lub)/60 + second(tm1.lub)/3600
+tm1.dechr
+force_tz(tm1.lub, "Pacific/Auckland")
+
+
+# Gerando um dataframe de datas
+sono <- data.frame(bed.time = ymd_hms("2013-09-01 23:05:24", "2013-09-02 22:51:09", 
+                                       "2013-09-04 00:09:16", "2013-09-04 23:43:31", "2013-09-06 00:17:41", "2013-09-06 22:42:27", 
+                                       "2013-09-08 00:22:27"), rise.time = ymd_hms("2013-09-02 08:03:29", "2013-09-03 07:34:21", 
+                                                                                   "2013-09-04 07:45:06", "2013-09-05 07:07:17", "2013-09-06 08:17:13", "2013-09-07 06:52:11", 
+                                                                                   "2013-09-08 07:15:19"), sleep.time = dhours(c(6.74, 7.92, 7.01, 6.23, 6.34, 7.42, 6.45)))
+sono
+sono$eficiencia <- round(sono$sleep.time/(sono$rise.time - sono$bed.time) * 100, 1)
+sono
+
+
+# Gerando um plot a partir de datas
+par(mar = c(5, 4, 4, 4))
+plot(round_date(sono$rise.time, "day"), sono$eficiencia, type = "o", col = "blue", xlab = "Manhã", ylab = NA)
+par(new = TRUE)
+plot(round_date(sono$rise.time, "day"), sono$sleep.time/3600, type = "o", col = "red", axes = FALSE, ylab = NA, xlab = NA)
+axis(side = 4)
+mtext(side = 4, line = 2.5, col = "red", "Duração do Sono")
+mtext(side = 2, line = 2.5, col = "blue", "Eficiência do Sono")
