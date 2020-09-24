051520b94b7a6fcf5d6b67d7a2fb2803ec032558
fdAssis <fd07ass@gmail.com>
2020-09-24 20:50:54 -0300

    R from scratch/arithmetic_operators

---
+++ 0 - Learn R from scratch/arithmetic_operators.r
@@ -0,0 +1,47 @@
+# Using arithmetic operators on variables
+number1 <- 10
+number2 <- 3
+
+# Addition
+number1 + number2
+
+# Subtraction
+number1 - number2
+
+# Multiplication
+number1 * number2
+
+# Exponent 
+number1 ^ number2
+
+# Modulus
+number1 %% number2
+
+# Integer Division
+number1 %/% number2
+
+# Using arithmetic operators on vectors
+vector1 <- c(5, 10, 15)
+vector2 <- c(3, 6, 9)
+
+# Addition
+vector1 + vector2
+
+# Subtraction
+vector1 - vector2
+
+# Multiplication
+vector1 * vector2
+
+# Division 
+vector1 / vector2
+
+# Exponent 
+vector1 ^ vector2
+
+# Modulus
+vector1 %% vector2
+
+# Integer Division
+vector1 %/% vector2
+
