65a0f0e8406a84ec6ac209005eb0b49ba3ff66af
fdAssis <fd07ass@gmail.com>
2020-09-24 21:35:34 -0300

    R from scratch/relational_operators

---
+++ 0 - Learn R from scratch/relational_operators.r
@@ -0,0 +1,49 @@
+# Relational operators on numbers
+number1 <- 30
+number2 <- 3
+
+# Equal to
+number1 == number2
+
+# Not equal to
+number1 != number2
+
+# Less than
+number1 < number2
+
+# Les than equal to
+number1 <= number2
+
+# Greater than 
+number1 > number2
+
+# Greater than equal to
+number1 >= number2
+
+# Relational operators on vectors
+vector1 <- c(5, 10, 15)
+vector2 <- c(3, 6, 9)
+
+# Equal to
+vector1 == vector2
+
+# Not equal to
+vector1 != vector2
+
+# Less than 
+vector1 < vector2
+
+# Less than equal to
+vector1 <= vector2
+
+# Greater than
+vector1 > vector2
+
+# Greater than equal to
+vector1 >= vector2
+
+# Check whether the number is in the vector
+myVector <- c(5, 3, 15)
+number <- 3
+
+number %in% myVector
