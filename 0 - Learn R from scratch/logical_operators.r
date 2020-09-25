70d7272e72905e33851d2e62af4e1681aa31ebd4
fdAssis <fd07ass@gmail.com>
2020-09-24 22:10:19 -0300

    R from scratch/logical_operators

---
+++ 0 - Learn R from scratch/logical_operators.r
@@ -0,0 +1,22 @@
+# Usage of logical operators
+number1 <- 10
+number2 <- 3
+
+# Element wise logical AND between number1 and number2
+number1 & number2 
+
+# Logical AND between number1 and number2
+number1 && number2
+
+# Element wise logical OR between number1 and number2
+number1 | number2
+
+# Element wise logical OR between number1 and number2
+number1 || number2
+
+# Not of number1
+!number1
+
+# XOR of number1 and number2 (AND > NOR > OR)
+xor(number1, number2) 
+
